ce3da13ebb63df1bcec027e9818540521fad804e
Zdeněk Drahoš (zdenekdrahos) <drahoszdenek@gmail.com>
2015-02-05 20:55:32 +0100

    Fix time formatter when result is 0s

--- tests/GivenTimeFormaterTest.php
+++ tests/GivenTimeFormaterTest.php
@@ -18,6 +18,7 @@ class GivenTimeFormaterTest extends \PHPUnit_Framework_TestCase
             array(65, 2, '1m 5s'),
             array(86922.2, 1, '1d 8m 42.2s'),
             array(12805.9, 0, '3h 33m 25s'),
+            array(0.000001, 2, '0s'),
         );
     }
 }
